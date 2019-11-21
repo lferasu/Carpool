4a27fe8ad2dbf1e3dfebf49ef46bdbe251d498f3
Sod-Sherman <sod.sherman@gmail.com>
2019-11-21 04:06:10 -0600

    notification fixing

--- notificationService/notficationListner/src/main/java/com/mum/carpool/notificationlistner/Listner/NotificationListner.java
+++ notificationService/notficationListner/src/main/java/com/mum/carpool/notificationlistner/Listner/NotificationListner.java
@@ -37,41 +37,46 @@ public class NotificationListner {
         this.emailConfiguration = emailConfiguration;
     }
 
-   // private EmailConfiguration emailConfiguration;
+    // private EmailConfiguration emailConfiguration;
 
-        @KafkaListener(topics = "${kafka.topic.newreservation}", groupId = "group_id")
-        public void consumeTrip (@Payload Trip trip,
-                                 @Headers MessageHeaders headers)  {
+    //        @KafkaListener(topics = "${kafka.topic.newreservation:NEWRESERVATION}", groupId = "group_id")
+    @KafkaListener(topics = "NEWRESERVATION", groupId = "group_id")
+    public void consumeTrip(@Payload Trip trip,
+                            @Headers MessageHeaders headers) {
+//            System.out.println("topics ===========> " + topics);
 
-            //Extract notification
-            List<Notification> notifications = NotificationUtil.extractNotification(trip);
-            // save notification to database
-            if(notifications!=null) {
-                notificationService.saveTripNotification(notifications);
-                //send email
-                List<Notification> filteredNotificationList = notifications.stream().filter(not->not.getEmail()!=null).collect(Collectors.toList());
-                filteredNotificationList.forEach(this::sendEmail);
-                //push kafka notification
-                notifications.forEach(notificationService::sendKafkaNotification);
-            }
+        //Extract notification
+        List<Notification> notifications = NotificationUtil.extractNotification(trip);
+        // save notification to database
+        if (notifications != null) {
+            notificationService.saveTripNotification(notifications);
+            //send email
+            List<Notification> filteredNotificationList = notifications.stream().filter(not -> not.getEmail() != null).collect(Collectors.toList());
+            System.out.println("notifications ================================== " + notifications);
+            filteredNotificationList.forEach(this::sendEmail);
+            //push kafka notification
+            notifications.forEach(notificationService::sendKafkaNotification);
+        } else
+            System.out.println("notifications null =========================!!!!!!!!!!!!!111 ");
 
-        }
+    }
 
-        private void sendEmail(Notification notification) {
-            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-            mailSender.setHost("smtp.mailtrap.io");
-            mailSender.setPort(25);
-            mailSender.setUsername("7ebd686d62d48b");
-            mailSender.setPassword("258180aa6560e6");
-            //Create an email instance
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom(adminEmail);
-            mailMessage.setTo(notification.getEmail());
-            mailMessage.setSubject(mailSubject + " : " + notification.getNotificationType());
-            mailMessage.setText(notification.getMessage());
-            // Send email
-            mailSender.send(mailMessage);
-        }
+    private void sendEmail(Notification notification) {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.mailtrap.io");
+        mailSender.setPort(25);
+        mailSender.setUsername("7ebd686d62d48b");
+        mailSender.setPassword("258180aa6560e6");
+        //Create an email instance
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setFrom(adminEmail);
+        mailMessage.setTo(notification.getEmail());
+        mailMessage.setSubject(mailSubject + " : " + notification.getNotificationType());
+        mailMessage.setText(notification.getMessage());
+        // Send email
+        System.out.println("email is sending mailMessage ================= " + mailMessage);
+        mailSender.send(mailMessage);
+    }
 }
 
 
