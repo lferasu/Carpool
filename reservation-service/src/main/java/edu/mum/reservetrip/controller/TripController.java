8e251e45fe9c754273717cf5495a1437f77f1487
Sod-Sherman <sod.sherman@gmail.com>
2019-11-21 03:07:27 -0600

    reservation deployment

--- reservation-service/src/main/java/edu/mum/reservetrip/controller/TripController.java
+++ reservation-service/src/main/java/edu/mum/reservetrip/controller/TripController.java
@@ -7,7 +7,7 @@ import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-//@CrossOrigin("*")
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/travel")
 public class TripController {
