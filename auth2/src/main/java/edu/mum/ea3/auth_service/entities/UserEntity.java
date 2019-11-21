eb8c7906ce4b47774356cebda2b3c58ac70c1e93
GetanehYilma <gyletike@mum.edu>
2019-11-20 21:10:27 -0600

    zipkin server is added

---
+++ auth2/src/main/java/edu/mum/ea3/auth_service/entities/UserEntity.java
@@ -0,0 +1,58 @@
+package edu.mum.ea3.auth_service.entities;
+
+//import edu.mum.shared.models.User;
+import lombok.*;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.UUID;
+
+//import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Table("users")
+public class UserEntity {
+
+//    private final long idInitial = 0;
+
+    @PrimaryKeyColumn(name = "id", type = PrimaryKeyType.PARTITIONED, ordering = Ordering.DESCENDING)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String password;
+   // @PrimaryKeyColumn(name = "email", type = PrimaryKeyType.PARTITIONED, ordering = Ordering.DESCENDING)
+    private String email;
+
+    // address
+    private String adrStreet;
+    private String adrCity;
+    private String adrState;
+    private String adrZip;
+
+
+    public UserEntity toUserModel(){
+        UserEntity user = new UserEntity();
+        user.setId(this.id);
+        user.setFirstName(this.firstName);
+        user.setLastName(this.lastName);
+        user.setEmail(this.email);
+
+        user.setAdrStreet(this.adrStreet);
+        user.setAdrCity(this.adrCity);
+        user.setAdrState(this.adrState);
+        user.setAdrZip(this.adrZip);
+
+        user.setAdrZip(this.adrZip);
+
+        return user;
+    }
+
+
+}
---
+++ zipkin-server/src/test/java/edu/mum/zipkinserver/ZipkinServerApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.mum.zipkinserver;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZipkinServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
